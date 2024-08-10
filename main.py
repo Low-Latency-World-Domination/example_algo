0749ab401d1686d1954c95aae2f7aaf2d5962f5f
stevenmaharaj <stevesamah@gmail.com>
2024-08-10 13:30:19 +1000

    SP-6 Added trader service

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 from config import load_config
-from fed_messages_pb2 import Exchange, Side
+from fed_messages_pb2 import Exchange, Side, OrderType
 from portfolio_manager_pb2 import Fill
 from trader_pb2 import Order
+import trader_pb2_grpc
+import portfolio_manager_pb2_grpc
+import grpc
 
 
 def main():
     yaml_file_path = './config.yaml'
     configs = load_config(yaml_file_path)
 
+
+    # connect to grpc server
+    trader_channel = grpc.insecure_channel('localhost:50051')
+    trader_stub = trader_pb2_grpc.TraderStub(trader_channel)
 
     
 
@@ -21,14 +28,22 @@ def main():
         price = fill.price + 2.0 * spread  
         side = Side.SELL 
 
-        order = Order()
-
     else:
         spread = configs[fill.symbol].spreadbps * 1000.0
         price = fill.price - 2.0 * spread  
         side = Side.BUY 
 
-        order = Order()
+    order = Order(
+        exchange=fill.exchange,
+        symbol=fill.symbol,
+        side=side,
+        price=price,
+        amount=fill.amount,
+        order_type=OrderType.LIMIT
+        )
+
+
+    resp = trader_stub.SubmitOrder(order)
 
 
 
